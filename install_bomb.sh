7e7d7fae7115a7997ff54ac2714402290c3b123a
black.dragon74 <nickk.2974@gmail.com>
2018-02-22 20:55:11 +0530

    Complete installation script
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- install_bomb.sh
+++ install_bomb.sh
@@ -6,9 +6,14 @@
 # Script to install the timebomb
 # Must be executed from the directory of bomb.sh
 
+# Declare Vars
+userDir=$(echo $HOME)
+userLib=$userDir/Library
+installLoc="/bin"
+
 # Get root access
 echo "Installing the timebomb..."
-echo "Please enter the administrator's password"
+echo "Getting root elevation..."
 
 # If root access failed, you should exit
 isRoot=$(sudo id -u 2>/dev/null)
@@ -16,4 +21,28 @@ if [[ $isRoot == "0" ]]; then
 	echo "Root access gained"
 else
 	echo "Root elevation failed. Aborting" && exit
+fi
+
+# Change directory to script execution dir and check for the required files
+cd $(dirname $0)
+
+# Check for bomb.sh and daemon
+if [[ -e bomb.sh && -e org.nick.bombd.plist ]]; then
+	echo "Installing files..."
+	sudo cp bomb.sh $installLoc/tbomb
+	cp org.nick.bombd.plist $userLib/LaunchAgents
+
+	echo "Setting permissions..."
+	sudo chmod a+x $installLoc/tbomb
+	chmod 755 $userLib/LaunchAgents/org.nick.bombd.plist
+
+	echo "Loading daeomn..."
+	launchctl load $userLib/LaunchAgents/org.nick.bombd.plist
+
+	echo "Done"
+
+	exit 0
+else
+	echo "Required files not found, make sure you are running from the same dir as bomb.sh"
+	exit 1
 fi
